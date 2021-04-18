e33b6fea4bf6471a7f3d3f9900c814e6f992ae19
Joseph Siracusa <>
2021-04-18 23:45:32 +0000

    1. Added CORS dependency to support cross-origin requests
    2. Created route to verify email from client contact form

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
+var cors = require('cors')
+var app = express();
+
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/verify-email', (req, res) => {
+  let name = req.query.name
+  let email = req.query.email
+  let message = req.query.message
+
+  res.send({isValid: isValid(name, email, message)})
 })
+
+app.listen(3000, () => {
+  console.log("App Listening At http://Marketing-Website-Server.obese_clown.repl.co");
+})
+
+function isValid(name, email, message)
+{
+  let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+  if (name === "")
+  {
+    return false;
+  }
+ 
+  if (!(email.match(mailformat)))
+  {
+    return false;
+  }
+
+  if (message === "")
+  {
+    return false;
+  }
+  return true;
+}
